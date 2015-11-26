0186be8658abfb77545498806890266820172469
jimmy.fortin@gmail.com <jimmy.fortin@gmail.com>
2015-11-26 16:34:13 -0500

    build task completed

---
+++ tasks/build.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var path = require('path');
+
+var $ = require('./utils/plugins-loader');
+
+module.exports = function (config, gulp) {
+    gulp.task('partials', function () {
+        var sources = [
+            path.join(config.paths.src, '/app/**/*.html'),
+            path.join(config.paths.tmp, '/serve/app/**/*.html')
+        ];
+        
+        return gulp.src(sources)
+            .pipe($.minifyHtml({
+                empty: true,
+                spare: true,
+                quotes: true
+            }))
+            .pipe($.angularTemplatecache('templateCacheHtml.js', {
+                module: config.angular.module,
+                root: 'app'
+            }))
+            .pipe(gulp.dest(config.paths.tmp + '/partials/'));
+    });
+    
+    gulp.task('html', ['inject', 'partials'], function () {
+        var partialsInjectFile = gulp.src(path.join(config.paths.tmp, '/partials/templateCacheHtml.js'), { read: false });
+        var partialsInjectOptions = {
+            starttag: '<!-- inject:partials -->',
+            ignorePath: path.join(config.paths.tmp, '/partials'),
+            addRootSlash: false
+        };
+        
+        var htmlFilter = $.filter('*.html', { restore: true });
+        var jsFilter = $.filter('**/*.js', { restore: true });
+        var cssFilter = $.filter('**/*.css', { restore: true });
+        var assets;
+        
+        return gulp.src(path.join(config.paths.tmp, '/serve/*.html'))
+            .pipe($.inject(partialsInjectFile, partialsInjectOptions))
+            .pipe(assets = $.useref.assets())
+            .pipe($.rev())
+            .pipe(jsFilter)
+            .pipe($.sourcemaps.init())
+            .pipe($.ngAnnotate())
+            .pipe($.uglify({ preserveComments: $.uglifySaveLicense }))
+            .pipe($.sourcemaps.write('maps'))
+            .pipe(jsFilter.restore)
+            .pipe(cssFilter)
+            .pipe($.sourcemaps.init())
+            .pipe($.minifyCss({ processImport: false }))
+            .pipe($.sourcemaps.write('maps'))
+            .pipe(cssFilter.restore)
+            .pipe(assets.restore())
+            .pipe($.useref())
+            .pipe($.revReplace())
+            .pipe(htmlFilter)
+            .pipe($.minifyHtml({
+                empty: true,
+                spare: true,
+                quotes: true,
+                conditionals: true
+            }))
+            .pipe(htmlFilter.restore)
+            .pipe(gulp.dest(path.join(config.paths.dist, '/')))
+            .pipe($.size({ title: path.join(config.paths.dist, '/'), showFiles: true }));
+    });
+    
+    // Only applies for fonts from bower dependencies
+    // Custom fonts are handled by the "other" task
+    gulp.task('fonts', function () {
+        return gulp.src($.mainBowerFiles())
+            .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
+            .pipe($.flatten())
+            .pipe(gulp.dest(path.join(config.paths.dist, '/fonts/')));
+    });
+    
+    gulp.task('other', function () {
+        var fileFilter = $.filter(function (file) {
+            return file.stat.isFile();
+        });
+        
+        var sources = [
+            path.join(config.paths.src, '/**/*'),
+            path.join('!' + config.paths.src, '/**/*.{html,css,js}')
+        ];
+        
+        return gulp.src(sources)
+            .pipe(fileFilter)
+            .pipe(gulp.dest(path.join(config.paths.dist, '/')));
+    });
+    
+    gulp.task('clean', function () {
+        return $.del([path.join(config.paths.dist, '/'), path.join(config.paths.tmp, '/')]);
+    });
+    
+    gulp.task('build', ['html', 'fonts', 'other']);
+}
--- tasks/index.js
+++ tasks/index.js
@@ -7,6 +7,9 @@ module.exports = function (options, gulp) {
     }
     
     var config = {
+        angular: {
+            module: options.angular.module
+        },
         paths: {
             src: 'src',
             tmp: '.tmp',
@@ -16,6 +19,7 @@ module.exports = function (options, gulp) {
 
     require('./scripts')(config, gulp);
     require('./inject')(config, gulp);
+    require('./build')(config, gulp);
     
 	gulp.task('test', function () {
 		$.util.log('test called - not implemented yet', options);
@@ -24,7 +28,10 @@ module.exports = function (options, gulp) {
 	gulp.task('test:e2e', function () {
 		$.util.log('test:e2e called - not implemented yet', options);
 	});
-	
+    
+    // Used by Visual Studio Code to run debugger
+    gulp.task('start', ['serve']);
+    
 	gulp.task('serve', function () {
 		$.util.log('serve called - not implemented yet', options);
 	});
@@ -32,8 +39,4 @@ module.exports = function (options, gulp) {
 	gulp.task('serve:dist', function () {
 		$.util.log('serve:dist called - not implemented yet', options);
 	});
-	
-	gulp.task('build', ['scripts'], function () {
-        $.util.log('build called - not fully implemented yet', options);
-    });
 }
