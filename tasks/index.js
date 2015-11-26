18fd086f487e9235ad0102aaa297c21ea6b92568
jimmy.fortin@gmail.com <jimmy.fortin@gmail.com>
2015-11-26 04:51:44 -0500

    New project with unimplemented tasks

---
+++ tasks/index.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+
+module.exports = function (options) {
+	gulp.task('test', function () {
+		console.log('test called - not implemented yet', options);
+	});
+	
+	gulp.task('test:e2e', function () {
+		console.log('test:e2e called - not implemented yet', options);
+	});
+	
+	gulp.task('serve', function () {
+		console.log('serve called - not implemented yet', options);
+	});
+	
+	gulp.task('serve:dist', function () {
+		console.log('serve:dist called - not implemented yet', options);
+	});
+	
+	gulp.task('build', function () {
+		console.log('build called - not implemented yet', options);
+	});
+}
